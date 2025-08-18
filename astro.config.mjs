0d95a8b66f80c998bb9841c0ab6053b6d3738c6a
Ddos-spec <setgraph69@gmail.com>
2025-08-18 10:20:29 +0700

    kirm

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jasalasercutting.com',
-  base: '/jasalasercutting', // Sesuaikan dengan path di kode Anda
   
   integrations: [tailwind(), sitemap()],
   
